60b818017519a09ec34baf07a1e122f3674be72b
unknown <alexmeys@gmail.com>
2021-10-16 21:35:37 +0200

    Added option 3

--- NutanixJsonInv.py
+++ NutanixJsonInv.py
@@ -1,101 +1,119 @@
 #!/usr/bin/python3
 # nutanixInv.py
 
+# pip install glob2 in case you don't have the module.
+# 1) Export from Nutanix (Prism Element) console > VM > Json
+# 2) Copy the files in the same directory as this script
+# 3) run the script. 
+
 import json
 import glob2
 from difflib import get_close_matches
 
-
-# finds data in current folder and opens it
-def findfiles(opt):
-    wvm = ""
+# finds data in current folder, opens it and returns the content
+def openfile():
     l_files = glob2.glob("*.json")
+    data = []
     for item in l_files:
-        data = json.load(open(item))
-        if opt == 1:
-            print(1)
-        elif opt == 2:
-            print(2)
-        elif opt == 3:
-            if wvm == "":
-                wvm = input("Enter your machine name: ")
-                for mitem in data:
-                    if wvm == mitem['VM Name']:
-                        print(mitem['VM Name'])
-
-                    elif len(get_close_matches(wvm, mitem.keys()))>0:
-                        guess = input("Did you mean %s instead (y/n)? " % get_close_matches(wvm, mitem.keys())[0])
-                        if guess.lower() == "yes" or guess.lower() == "y":
-                            print(mitem['VM Name'])
-                    
-                    elif len(get_close_matches(wvm, mitem.keys()))<=0:
-                        print("no items to be found")
-
-            else:
-                for mitem in data:
-                    if wvm == mitem['VM Name']:
-                        print(mitem['VM Name'])
+        data.append(json.load(open(item)))
         
-        else:
-            print("What are you even doing here?")
+    return data
 
-"""
-def render():
-                    for mitem in data:
-                    if wvm == mitem['VM Name']:
-                        print("\nMachine found: \n")
-                        print("Name: ", mitem['VM Name'])
-                        print("IP: ", mitem['IP Addresses'])
-                        print("CPU Cores: ", mitem['Cores'])
-                        print("Ram: ", mitem['Memory Capacity'])
-                        print("Flash: ", mitem['Flash Mode'])
-                        print("\nResources currently in use: \n")
-                        print("CPU Usage: ", mitem['CPU Usage'])
-                        print("Ram Usage: ", mitem['Memory Usage'])
-                        print("Disk usage: ", mitem['Storage'])
-                        print("Write IOPS: ", mitem['Controller Write IOPS'])
-                        print("Avg IO Latency", mitem['Controller Avg IO Latency'])
-                        print("\n")
 
-"""                        
-# specifying data
-def finetuning(opt):
+# get into the options and work with the data
+def handleoptions(opt, data):
     if opt == 1:
-        print("Inventory printing\n")
-        my_files = findfiles()
-        getsrcdata(my_files)
+        i = 0
+        for sepfiles in data:
+            for lines in sepfiles:
+                print(lines['VM Name'])
+                i +=1
+        print("Total items: ", i)
+
     elif opt == 2:
-        print("Resource printing\n")
+        findme = input("Enter your device name: ")
+        lmachines = []
+        result = False
+        for sepfiles in data:
+            for lines in sepfiles:
+                lmachines.append(lines['VM Name'])
+                if (findme == lines['VM Name']) or (findme.lower() in lines['VM Name']) or (findme.capitalize() in lines['VM Name']):
+                    result = True
+                    print("\nMachine found: \n")
+                    print("Name: ", lines['VM Name'])
+                    print("IP: ", lines['IP Addresses'])
+                    print("CPU Cores: ", lines['Cores'])
+                    print("Ram: ", lines['Memory Capacity'])
+                    print("Flash: ", lines['Flash Mode'])
+                    print("\nResources currently in use: \n")
+                    print("CPU Usage: ", lines['CPU Usage'])
+                    print("Ram Usage: ", lines['Memory Usage'])
+                    print("Disk usage: ", lines['Storage'])
+                    print("Write IOPS: ", lines['Controller Write IOPS'])
+                    print("Avg IO Latency", lines['Controller Avg IO Latency'])
+                    print("\n")
+
+        if not result:    
+            if get_close_matches(findme,lmachines):
+                print("Could not find the name you mentioned... I found this one: %s" % get_close_matches(findme,lmachines)[0])
+                opts = input("Is this the one you are searching for (y/n) ")
+                if opts.lower() == 'y' or opts.lower() == "yes":
+                    findme = get_close_matches(findme,lmachines)[0]
+                    for sepfiles in data:
+                        for lines in sepfiles:
+                            if findme == lines['VM Name']:
+                                print("\nMachine found: \n")
+                                print("Name: ", lines['VM Name'])
+                                print("IP: ", lines['IP Addresses'])
+                                print("CPU Cores: ", lines['Cores'])
+                                print("Ram: ", lines['Memory Capacity'])
+                                print("Flash: ", lines['Flash Mode'])
+                                print("\nResources currently in use: \n")
+                                print("CPU Usage: ", lines['CPU Usage'])
+                                print("Ram Usage: ", lines['Memory Usage'])
+                                print("Disk usage: ", lines['Storage'])
+                                print("Write IOPS: ", lines['Controller Write IOPS'])
+                                print("Avg IO Latency", lines['Controller Avg IO Latency'])
+                                print("\n")
+                else:
+                    print("\nHmm, these are the ones I found, try the program again with one of those names:")
+                    foundall = get_close_matches(findme,lmachines,5)
+                    for fitem in foundall:
+                        print(fitem)
+
+
     elif opt == 3:
-        print("Find a vm")
-        findfiles(3)
-    else:
-        print("What are you even doing here?")
-
-
-
+        for sepfiles in data:
+            for lines in sepfiles:
+                if not lines['IP Addresses']:
+                    print("Offline: ", lines['VM Name'])
+        
+                   
+# Start Program
 # Iterate until valid Menu option is chosen
 opt = 'nope'
 while type(opt) != int:
     print("*** Menu ***")
     print("1. Print inventory")
-    print("2. Print resources")
-    print("3. Find a VM")
+    print("2. Find a VM")
+    print("3. Print Offline VM's")
     print("4. Exit")
     opt = input("Pick an option: ")
     try:
         opt = int(opt)
-    except:
+    except ValueError:
         print("\nThat's not a valid option\n")
         opt = 'nope'
 
+# Open file content in this variable from return
+content = openfile()
+
+# Pass by pointer?
 if opt == 1:
-    finetuning(1)
+    handleoptions(1, content)
 elif opt == 2:
-    finetuning(2)
+    handleoptions(2, content)
 elif opt == 3:
-    finetuning(3)
+    handleoptions(3, content)
 else:
     print("... Exiting ...")
-
-
